2871b780a55851ab57a9c7620cceec94f61b722c
besnovatyj <metr31@yandex.ru>
2026-06-15 02:56:35 +0500

    to new modman

--- src/Module.php
+++ src/Module.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /*
  * Copyright (c) 2026 Besnovatyj. Licensed under the MIT License.
  */
@@ -9,38 +8,33 @@ declare(strict_types=1);
 
 namespace Besnovatyj\Page;
 
-use common\components\module\BaseModule;
+use common\components\module\CmsModule;
+use modules\modmanNew\contract\DeclaresModule;
+use modules\modmanNew\contract\ProvidesAdminMenu;
+use modules\modmanNew\contract\ProvidesDependencies;
+use modules\modmanNew\contract\ProvidesDirectories;
+use modules\modmanNew\contract\ProvidesMigrations;
 
 /**
  * Модуль управления статическими страницами
  */
-class Module extends BaseModule
+class Module extends CmsModule implements
+    DeclaresModule, ProvidesAdminMenu,
+    ProvidesDependencies, ProvidesDirectories,
+    ProvidesMigrations
 {
     public const bool EDITABLE = true;
     public const string VERSION = '2.0.0';
+    public const string MODULE_ID = 'Page';
 
-    public static function getAdminMenu(): array
-    {
-        return require __DIR__ . '/config/adminMenu.php';
-    }
+    public static function moduleId(): string { return self::MODULE_ID; }
+    public static function moduleVersion(): string { return self::VERSION; }
+    public static function isEditable(): bool { return self::EDITABLE; }
+    public static function adminMenu(): array { return require __DIR__.'/config/adminMenu.php'; }
+    public static function moduleConfig(): array { return require __DIR__.'/config/config.php'; }
+    public static function dependencies(): array { return require __DIR__.'/config/dependencies.php'; }
+    public static function migrationPath(): string { return __DIR__.'/migrations'; }
+    public static function migrationNamespace(): ?string { return __NAMESPACE__.'\\migrations'; }
+    public static function directories(): array { return ['@static/origin/Page','@static/cache/Page'];}
 
-    public static function getConfig(): array
-    {
-        return require __DIR__ . '/config/config.php';
-    }
-
-    public static function getOptions(): array
-    {
-        return require __DIR__ . '/config/options.php';
-    }
-
-    public static function getDependencies(): array
-    {
-        return require __DIR__ . '/config/dependencies.php';
-    }
-
-    public static function setContainerConfig(): void
-    {
-        (require __DIR__ . '/config/container.php')(\Yii::$container);
-    }
 }
