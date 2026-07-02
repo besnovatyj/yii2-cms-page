e2eed6da2dc9de9d36203c761288b9de3819819f
besnovatyj <metr31@yandex.ru>
2026-07-02 21:52:35 +0500

    extract modules contracts

--- src/Module.php
+++ src/Module.php
@@ -9,11 +9,11 @@ declare(strict_types=1);
 namespace Besnovatyj\Page;
 
 use common\components\module\CmsModule;
-use modules\modman\contract\DeclaresModule;
-use modules\modman\contract\ProvidesAdminMenu;
-use modules\modman\contract\ProvidesDependencies;
-use modules\modman\contract\ProvidesDirectories;
-use modules\modman\contract\ProvidesMigrations;
+use Besnovatyj\Contracts\module\DeclaresModule;
+use Besnovatyj\Contracts\module\ProvidesAdminMenu;
+use Besnovatyj\Contracts\module\ProvidesDependencies;
+use Besnovatyj\Contracts\module\ProvidesDirectories;
+use Besnovatyj\Contracts\module\ProvidesMigrations;
 
 /**
  * Модуль управления статическими страницами
